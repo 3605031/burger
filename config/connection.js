da4c60dda0c14b56c786206c5d8c19eb3aea8398
Blake Desktop <bhhuynh@ucsd.edu>
2017-07-29 00:36:13 -0700

    heroku credentials

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "Soithan1995",
 	database: "burgers_db"
-});
+});}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
